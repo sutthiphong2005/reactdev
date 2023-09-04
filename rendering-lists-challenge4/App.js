32c88e56914642c11cde8e87c05677b822a080af
sutthiphong2005 <sutthiphong2005@gmail.com>
2023-09-04 22:01:16 +0700

    App.js

---
+++ rendering-lists-challenge4/App.js
@@ -0,0 +1,28 @@
+const poem = {
+  lines: [
+    'I write, erase, rewrite',
+    'Erase again, and then',
+    'A poppy blooms.',
+    'Erase again, and then',
+    'A poppy blooms.'    
+  ]
+};
+
+export default function Poem() {
+  const x = 5;
+  return (
+    <article>
+      {poem.lines.map((line, index) => {
+
+        if( index < poem.lines.length && index != poem.lines.length-1){
+          return <div key={index}><p key={index}>{line}</p><hr/></div>
+        }else{
+          return <div key={index}><p>{line}</p></div>
+        }
+
+        
+      } 
+      )}
+    </article>
+  );
+}
